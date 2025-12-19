74b26b6a493a519490d254eb4962f8f9bdb021a6
pwalczysko <p.walczysko@dundee.ac.uk>
2025-12-19 15:25:33 +0000

    Apply suggestion from @khaledk2
    Co-authored-by: khaledk2 <khaledk2@hotmail.com>

--- src/omero_demo_cleanup/library.py
+++ src/omero_demo_cleanup/library.py
@@ -333,7 +333,7 @@ def resource_usage(
                 if who.first == user_id:
                     file_size += usage
         except Exception as ex:
-            print(f'FAILED to count data for "{user_name}" (#{user_id}).')
+            print(f'FAILED to count data for "{user_name}" (#{user_id}), error message is {ex}.')
 
         if file_count > 0 or file_size > 0:
             user_stats.append(
