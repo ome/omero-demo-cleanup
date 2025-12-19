6aec23082a01e734851c11b0b3738f31d86070ce
khaledk2 <khaledk2@hotmail.com>
2025-12-19 16:29:14 +0000

    Update src/omero_demo_cleanup/library.py

--- src/omero_demo_cleanup/library.py
+++ src/omero_demo_cleanup/library.py
@@ -335,7 +335,6 @@ def resource_usage(
         except Exception as ex:  # noqa
             print(
                 f'FAILED to count data for "{user_name}" (#{user_id}), '
-                f 'error message is {ex}.'
             )
 
         if file_count > 0 or file_size > 0:
